278298aac4afbd4aa46cc0b28df5eb86685d13e7
jorge gangale <jorge.gangale@gmail.com>
2019-07-15 10:27:41 -0400

    cambios

--- js/freelancer.js
+++ js/freelancer.js
@@ -64,6 +64,7 @@
 
 
 
+/*--------loader script-----------*/
 $(function(){
     var loading = $('#loadbar').hide();
     $(document)
@@ -73,131 +74,149 @@ $(function(){
     	loading.hide();
     });
     
-    $("label.btn").on('click',function () {
-    	var choice = $(this).find('input:radio').val();
-    	$('#loadbar').show();
-    	$('#quiz').fadeOut();
-    	setTimeout(function(){
-           $( "#answer" ).html(  $(this).checking(choice) );      
-            $('#quiz').show();
-            $('#loadbar').fadeOut();
-           /* something else */
-    	}, 1500);
+    var questionNo = 0;
+    var correctCount = 0;
+    var q = [
+        {'Q':'How do you write "Hello World" in an alert box?', 'A':2,'C':['msg("Hello World");','alert("Hello World");','alertBox("Hello World");']},
+        {'Q':'How do you create a function in JavaScript?', 'A':3,'C':['function:myFunction()','function = myFunction()','function myFunction()']},
+        {'Q':'How to write an IF statement in JavaScript?', 'A':1,'C':['if (i == 5)','if i = 5 then','if i == 5 then']},
+        {'Q':'How does a FOR loop start?', 'A':2,'C':['for (i = 0; i <= 5)','for (i = 0; i <= 5; i++)','for i = 1 to 5']},
+        {'Q':'What is the correct way to write a JavaScript array?', 'A':3,'C':['var colors = "red", "green", "blue"','var colors = (1:"red", 2:"green", 3:"blue")','var colors = ["red", "green", "blue"]']}
+    ];
+
+ 
+    $(document.body).on('click',"label.element-animation",function (e) {
+    //ripple start
+        var parent, ink, d, x, y;    	
+         parent = $(this);
+        if(parent.find(".ink").length == 0)
+            parent.prepend("<span class='ink'></span>");
+            
+        ink = parent.find(".ink");
+        ink.removeClass("animate");
+        
+        if(!ink.height() && !ink.width())
+        {
+            d = Math.max(parent.outerWidth(), parent.outerHeight());
+            ink.css({height: "100px", width: "100px"});
+        }
+        
+         x = e.pageX - parent.offset().left - ink.width()/2;
+        y = e.pageY - parent.offset().top - ink.height()/2;
+        
+        ink.css({top: y+'px', left: x+'px'}).addClass("animate");
+    //ripple end
+
+        var choice = $(this).parent().find('input:radio').val();
+        console.log(choice);
+    	var anscheck =  $(this).checking(questionNo, choice);//$( "#answer" ).html(  );      
+        q[questionNo].UC = choice;
+        if(anscheck){
+            correctCount++;
+            q[questionNo].result = "Correct";
+        } else {
+            q[questionNo].result = "Incorrect";        
+        }
+        console.log("CorrectCount:" + correctCount);
+        setTimeout(function(){
+            $('#loadbar').show();
+            $('#quiz').fadeOut();
+            questionNo++;
+            if((questionNo + 1) > q.length){
+                alert("Quiz completed, Now click ok to get your answer");
+                $('label.element-animation').unbind('click');
+                setTimeout(function(){
+                    var toAppend = '';
+                    $.each(q, function(i, a){
+                        toAppend += '<tr>'
+                        toAppend += '<td>'+(i+1)+'</td>';
+                        toAppend += '<td>'+a.A+'</td>';
+                        toAppend += '<td>'+a.UC+'</td>';
+                        toAppend += '<td>'+a.result+'</td>';
+                        toAppend += '</tr>'
+                    });
+                    $('#quizResult').html(toAppend);
+                    $('#totalCorrect').html("Total correct: " + correctCount);
+                    $('#quizResult').show();
+                    $('#loadbar').fadeOut();
+                    $('#result-of-question').show();
+                    $('#graph-result').show();
+                    chartMake();
+                }, 1000);
+            } else {
+                $('#qid').html(questionNo + 1);
+                $('input:radio').prop('checked', false);
+                setTimeout(function(){
+                    $('#quiz').show();
+                    $('#loadbar').fadeOut();
+                }, 1500);
+                $('#question').html(q[questionNo].Q);
+                $($('#f-option').parent().find('label')).html(q[questionNo].C[0]);
+                $($('#s-option').parent().find('label')).html(q[questionNo].C[1]);
+                $($('#t-option').parent().find('label')).html(q[questionNo].C[2]);
+            }
+        }, 1000);
     });
 
-    $ans = 3;
-
-    $.fn.checking = function(ck) {
-        if (ck != $ans)
-            return 'INCORRECT';
+    
+    $.fn.checking = function(qstn, ck) {
+        var ans = q[questionNo].A;
+        if (ck != ans)
+            return false;
         else 
-            return 'CORRECT';
+            return true;
     }; 
+
+// chartMake();
+    function chartMake(){
+
+         var chart = AmCharts.makeChart("chartdiv",
+            {
+            "type": "serial",
+            "theme": "dark",
+            "dataProvider": [{
+                "name": "Correct",
+                "points": correctCount,
+                "color": "#00FF00",
+                "bullet": "http://i2.wp.com/img2.wikia.nocookie.net/__cb20131006005440/strategy-empires/images/8/8e/Check_mark_green.png?w=250"
+            }, {
+                "name": "Incorrect",
+                "points":q.length-correctCount,
+                "color": "red",
+                "bullet": "http://4vector.com/i/free-vector-x-wrong-cross-no-clip-art_103115_X_Wrong_Cross_No_clip_art_medium.png"
+            }],
+            "valueAxes": [{
+                "maximum": q.length,
+                "minimum": 0,
+                "axisAlpha": 0,
+                "dashLength": 4,
+                "position": "left"
+            }],
+            "startDuration": 1,
+            "graphs": [{
+                "balloonText": "<span style='font-size:13px;'>[[category]]: <b>[[value]]</b></span>",
+                "bulletOffset": 10,
+                "bulletSize": 52,
+                "colorField": "color",
+                "cornerRadiusTop": 8,
+                "customBulletField": "bullet",
+                "fillAlphas": 0.8,
+                "lineAlpha": 0,
+                "type": "column",
+                "valueField": "points"
+            }],
+            "marginTop": 0,
+            "marginRight": 0,
+            "marginLeft": 0,
+            "marginBottom": 0,
+            "autoMargins": false,
+            "categoryField": "name",
+            "categoryAxis": {
+                "axisAlpha": 0,
+                "gridAlpha": 0,
+                "inside": true,
+                "tickLength": 0
+            }
+        });
+    }
 });	
-=======
-  });
-
-
-  // final results slide
-  var $results_slide = $("<div>")
-    .attr("class", "item")
-    .attr("height", height + "px")
-    .appendTo($slides);
-
-  var $results_title = $('<h1>')
-    .attr('class', 'quiz-title')
-    .appendTo($results_slide);
-
-  var $results_ratio = $('<div>')
-    .attr('class', 'results-ratio')
-    .appendTo($results_slide);
-
-  var $restart_button = $("<div>")
-    .attr("class", "quiz-answers")
-    .appendTo($results_slide);
-
-  var $social = $("<div>")
-    .attr('class', 'results-social')
-    .html('<div id = "social-text">Did you like the quiz? Share your results with your friends, so they can give it a shot!</div>')
-    .appendTo($results_slide);
-
-  var $twitter_link = $('<a>')
-    .html('<span class="social social-twitter follow-tw"></span>')
-    .appendTo($social);
-
-  var $facebook_link = $('<a>')
-    .html('<span class="social social-facebook follow-fb"></span>')
-    .appendTo($social);
-
-  $("<button>")
-    .attr('class', 'quiz-button btn')
-    .text("Try again?")
-    .click(function() {
-      state.correct = 0;
-      $quiz.carousel(0);
-    })
-    .appendTo($restart_button);
-
-  $quiz.carousel({
-    "interval" : false
-  });
-
-  $(window).on('resize', function() {
-    $quiz.find(".item")
-      .attr('height', $quiz.height() + "px");
-  });
-
-}
-
-function resultsText(state) {
-
-  var ratio = state.correct / state.total;
-  var text;
-
-  switch (true) {
-    case (ratio === 1):
-      text = "Wow&mdash;perfect score!";
-      break;
-    case (ratio > 0.9):
-      text = "Awesome job, you got most of them right.";
-      break;
-    case (ratio > 0.60):
-      text = "Pretty good, we'll say that's a pass.";
-      break;
-    case (ratio > 0.5):
-      text = "Well, at least you got half of them right&hellip;";
-      break;
-    case (ratio < 0.5 && ratio !== 0):
-      text = "Looks like this was a tough one, better luck next time.";
-      break;
-    case (ratio === 0):
-      text = "Yikes, none correct. Well, maybe it was rigged?";
-      break;
-  }
-  return text;
-
-}
-
-
-function tweet(state, opts) {
-
-  var body = (
-    "I got " + state.correct +
-    " out of " + state.total +
-    " on @taxpolicycenter’s \"" + opts.title +
-    "\" quiz. Test your knowledge here: " + opts.url
-  );
-
-  return (
-    "http://twitter.com/intent/tweet?text=" +
-    encodeURIComponent(body)
-  );
-
-}
-
-function facebook(state, opts) {
-  return "https://www.facebook.com/sharer/sharer.php?u=" + opts.url;
-}
-
-
-})(jQuery);
