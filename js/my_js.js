adc3ff464ddefd5c7a77dd4c70ae2aa8edfbfa9a
jorge gangale <jorge.gangale@gmail.com>
2019-07-19 10:15:30 -0400

    agrgando

---
+++ js/my_js.js
@@ -0,0 +1,24 @@
+
+/* 
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+// A $( document ).ready() block.
+$(document).ready(function () {
+    $(".js-hidden").addClass("d-none");
+});
+
+$(".feedback-toggler").click(function () {
+    $id = $(this).attr("id");
+    $idClass = "#sec-" + $id;
+    if ($($idClass).hasClass('d-none')) {
+        $($idClass).removeClass("d-none");
+        $($idClass).addClass("d-inline");
+    } else {
+        $($idClass).removeClass("d-inline");
+        $($idClass).addClass("d-none");
+    }
+
+
+})
